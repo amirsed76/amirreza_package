302fe244d4c26cedaa5656be9f72a59703962334
amirsed76 <amirreza.seddighin1376@gmail.com>
2022-09-26 12:29:30 +0330

    ok

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='amirreza_package',
+    packages=['amirreza_package'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='https://github.com/amirsed76/amirreza_package',
+    author='amirreza',
+    author_email='amirreza.seddighin1376@gmail.com',
+    keywords=['pip', 'linode', 'example']
+)
