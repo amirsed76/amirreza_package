f4e57923ef5ea7d2f1f7ad73347db01d799e7d8c
amirsed76 <amirreza.seddighin1376@gmail.com>
2022-09-26 12:33:59 +0330

    ok

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='amirreza_package',
-    packages=['amirreza_package'],
+    packages=["numpy",'amirreza_package'],
     description='Hello world enterprise edition',
     version='0.1',
     url='https://github.com/amirsed76/amirreza_package',
